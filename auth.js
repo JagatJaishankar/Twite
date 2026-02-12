f48ff2ac8d54575a5edf475f57abac540665833f
JagatJaishankar <jagatj@outlook.com>
2026-02-12 21:52:09 +0530

    build: still works

--- auth.js
+++ auth.js
@@ -7,7 +7,7 @@ const config = {
   providers: [
     Resend({
       apiKey: process.env.RESEND_KEY,
-      from: "noreply@resend.creatorjag.at",
+      from: "hello@auth.jagatjaishankar.com",
       name: "Email",
     }),
   ],
